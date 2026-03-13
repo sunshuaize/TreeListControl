7b5b6dc3726829c29e25259149679f219fec022c
孙帅泽 <1157253646@qq.com>
2026-03-13 18:36:32 +0800

    更新启动入口为CustomTableAllFeaturesDemo演示窗体

--- demo/Program.cs
+++ demo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using demo.Controls.CustomTable;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace demo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new CustomTableAllFeaturesDemo());
         }
     }
 }
